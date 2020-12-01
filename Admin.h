74ecb07668b220498026c3637361f1a8449f3c12
ethan2151 <72755849+ethan2151@users.noreply.github.com>
2020-12-01 13:01:45 -0600

    Added Admin File
    Skeleton Admin file (pseudocode)

---
+++ Admin.h
@@ -0,0 +1,42 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+#include <string>
+#include <iostream>
+#include "Accounts.h"
+#include <limits>
+using namespace std;
+
+//Admin class represents the role of a banking administrator
+//Admin is a child of parent class Accounts
+
+class Admin : public Accounts
+{
+   public:
+      //public variables
+      
+   //Default constructor
+   Admin() : Accounts()
+   {
+   
+   }
+   //2nd constructor
+   Admin() : Accounts()
+   {
+   
+   }
+   
+   //function to display menu
+   
+   //function to create a bank official login
+
+   //function to enable a bank official login
+   
+   //function to disable a bank official login
+   
+   //function to create user account
+   
+   //function to delete user account
+   
+   //function to modify user account
+   
+   //function to change user password
