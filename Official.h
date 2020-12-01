00e443ae3a43a2e954f091215b20bd8da9d13f21
ett642 <74976855+ett642@users.noreply.github.com>
2020-12-01 13:29:32 -0600

    Super basic

---
+++ Official.h
@@ -0,0 +1,11 @@
+#ifndef OFFICIAL_H
+#define OFFICIAL_H
+#include <iostream>
+#include <string>
+
+class Official{
+public:
+	Official(int num, string pass, string inName, string date):Accounts(num,pass,inName,date){
+	}
+};
+#endif
