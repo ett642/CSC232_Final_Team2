9f1bd12d9299068b2d8917f768df28c010ec65f4
ett642 <74976855+ett642@users.noreply.github.com>
2020-12-01 20:34:17 -0600

    Update Official.h

--- Official.h
+++ Official.h
@@ -2,10 +2,43 @@
 #define OFFICIAL_H
 #include <iostream>
 #include <string>
+#include "Accounts.h"
+using namespace std;
 
-class Official{
+class Official : public Accounts{
+private:
+	string stat;
+	bool status;
+	
 public:
-	Official(int num, string pass, string inName, string date):Accounts(num,pass,inName,date){
+	Official(string num, string pass, string inName, string date, string s):Accounts(num,pass,inName,date){
+		stat = s;
+		createStatus(stat);
 	}
+	
+	void createStatus(string s){
+		if(s=="t")
+			status = true;
+		else
+			status = false;
+	}
+	
+	void loggedIn(string date){
+		if(status){
+			cout<<"Status: Active"<<endl;
+		}
+		else{
+			cout<<"Status: Inactive"<<endl;
+			return;
+		}
+	}
+	
+	//Method to open and close bank accounts
+	
+	//Method to retrieve info from closed accounts
+	
+	//deposit and withdraw from a user account with permission
+	
+	//Search Account by name, number, and phone number
 };
 #endif
