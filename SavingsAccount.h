f09608c008e3b0c0aafd38063f61b9f443db720e
lapis-subie6 <72754670+lapis-subie6@users.noreply.github.com>
2020-12-01 21:33:05 -0600

    Update SavingsAccount.h
    added active state/deactive state variable changed name of status to flag

--- SavingsAccount.h
+++ SavingsAccount.h
@@ -11,27 +11,27 @@ using namespace std;
 // AbAccount class
 
 class SavingsAccount : public AbAccount {
-    public:
-    bool status;
+   protected:
+      bool flag;
+   public:
    // Default constructor
    SavingsAccount() : AbAccount()
    {
-      status = true;
+      flag = true;
    }
    // 2nd Constructor
-   SavingsAccount (string num, double balance, double interestRate): 
-   AbAccount (num, balance, interestRate)
+   SavingsAccount (string num, string date, double balance, bool status, bool flag): AbAccount(num, date, balance,status)
    {
-      status = true;
+      flag = true;
    }
    //function to display menu
-   void display
+   //void display
    //function to deposit money
    void deposit(double amount) 
    {
       if(amount > 0.00) //if the amount deposited is more than 0 
       {
-         accBalance += amount;//successfully deposit
+         balance += amount;//successfully deposit
       }
       else //if the deposit is not a positive amount it will not deposit
       {
@@ -47,38 +47,34 @@ class SavingsAccount : public AbAccount {
             break;
          }
       }
-      if(accBalance >= 0.0) //if the balance is greater than $0 the status will remain open 
+      if(balance >= 0.0) //if the balance is greater than $0 the status will remain open 
       {
-         status = true;
-      }
-      else // redudant statement...
-      { 
-         status = false; 
+         flag = true;
       }
         
    }
    void withdraw(double amount) //function to withdraw money
    {
-       if(status == false) 
+       if(flag == false) 
         { // deny the withdraw
-            cout << "You may not withdraw, your account balance is: $" << accBalance << endl;
+            cout << "You may not withdraw, your account balance is: $" << balance << endl;
             cout << "You must raise your account balance above $0 to make a withdraw." << endl;
         }
-        else if (status == true) 
+        else if (flag == true) 
         { // do the withdraw from the balance
-            if(accBalance >= amount) 
+            if(balance >= amount) 
             {
-                accBalance -= amount; 
+                balance -= amount; 
             }
             else 
             {
                 cout << "Invalid funds " << endl;
             }
-            if(accBalance < 0.0)// if the balance falls below $0 account becomes inactive 
+            if(balance < 0.0)// if the balance falls below $0 account becomes inactive 
             {
                 cout << "Warning! You cannot withdraw your account has fallen below $0.00. " << endl;
                 cout << "You must deposit more money before you can make another withdraw." << endl;
-                status = false;
+                flag = false;
             }
         }
     }
