e2fa5c71a52cf13682e2eacb0f8af62bc9633fa6
lapis-subie6 <72754670+lapis-subie6@users.noreply.github.com>
2020-12-01 21:31:30 -0600

    Update AbAccount.h
    added setter function

--- AbAccount.h
+++ AbAccount.h
@@ -6,36 +6,29 @@ using namespace std;
 
 class AbAccount
 {
-    private:
-        string fName;
-        string lName;
-        string address;
+    protected:
         string accNum;
         double interestRate;
         int term;
         string dateOfOC;
         double balance;
         string records;
+        bool status;
 
     public:
         AbAccount()
         {
-            fName = "";
-            lName = "";
-            address = "";
-            accNum = "";
+            status = false;
             dateOfOC = "";
             balance = 0.00;
         }
 
-        AbAccount(string first, string last, string add, string acc, string date, double bal)
+        AbAccount( string acc, string date, double bal, bool stat)
         {
-            fName = first;
-            lName = last;
-            address = add;
             accNum = acc;
             dateOfOC = date;
             balance = bal;
+            status = stat;
 
         }
 
@@ -52,13 +45,30 @@ class AbAccount
         // calculates daily interest rate and adds the daily interest to account balance
         double calcInt(double balance)
         {
-            double dailyIR = annualIR / 365;
+            double dailyIR = interestRate / 365;
             dailyIR = dailyIR * balance;
             balance += dailyIR;
-            return accBalance;
+            return balance;
+        }
+
+        void setStatus()
+        {
+            int input;
+            cout << "1 to activate account.\n" << "2 to deactivate account.\n";
+            cin >> input;
+            switch(input)
+            {
+                case 1:
+                {
+                    status = true;
+                }
+                case 2:
+                {
+                    status = false;
+                }
+            }
         }
     
 
-}
-
+};
 #endif
