4b2369c449a835530ab359d65a7cc0323cc2809d
ett642 <74976855+ett642@users.noreply.github.com>
2020-12-01 20:37:45 -0600

    Don't think it will run as-is
    Too much unfinished stuff, but its a start

--- driver.cpp
+++ driver.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include "Accounts.h"
+#include "Admin.h"
+#include "Official.h"
+#include "User.h"
 using namespace std;
 
 /*
@@ -16,14 +21,71 @@ void getDate(string date, int *month, int *day, int *year){
 	*year = stoi(date);
 }
 
+void fromFile(vector<Accounts> &logins){
+	fstream file; string num; string pass; string name; string lastDate; string text;
+	file.open("logins.txt");
+	if(file){
+		while(getline(file,text)){
+			if(text[0] == '0'){
+				num = text.substr(0, text.find("*"));
+				text = text.substr(text.find("*")+1);
+				pass = text.substr(0, text.find("*"));
+				text = text.substr(text.find("*")+1);
+				name = text.substr(0, text.find("*"));
+				text = text.substr(text.find("*")+1);
+				lastDate = text;
+				Admin temp = Admin(num,pass,name,lastDate);
+				logins.push_back(temp);
+			}
+			else if(text[1] == '1'){
+				string status;
+				num = text.substr(0, text.find("*"));
+				text = text.substr(text.find("*")+1);
+				pass = text.substr(0, text.find("*"));
+				text = text.substr(text.find("*")+1);
+				name = text.substr(0, text.find("*"));
+				text = text.substr(text.find("*")+1);
+				lastDate = text.substr(0, text.find("*"));
+				text = text.substr(text.find("*")+1);
+				status = text;
+				Official temp = Official(num,pass,name,lastDate,status);
+				logins.push_back(temp);
+			}
+			else{
+				num = text.substr(0, text.find("*"));
+				text = text.substr(text.find("*")+1);
+				pass = text.substr(0, text.find("*"));
+				text = text.substr(text.find("*")+1);
+				name = text.substr(0, text.find("*"));
+				text = text.substr(text.find("*")+1);
+				lastDate = text;
+			}
+		}
+	}
+	file.close();
+}
+
+void loginSearch(string number, string password, vector<Accounts> &logins){
+	for(Account &i: logins){
+		if(number == i.getNumber() and password == i.getPassword){
+			cout<<"Logged in."<<endl;
+			return;
+		}
+	}
+	
+}
+
 main(){
-	int loginNum; string password; string date;
+	vector<Accounts> logins;
+	
+	string loginNum; string password; string date;
 	int month; int day; int year;
 	cout<<"Enter today's date (MM/DD/YYYY): ";
 	cin>>date;
 	getDate(date, &month, &day, &year);
+	fromFile(logins);
 	
-	cout<<"Welcome to Bear Bank!\n[1] Login\n[2] Create Account\n[3] Exit"<<endl;
+	cout<<"Welcome to Bear Bank!\n[1] Login\n[2] Exit"<<endl;
 	bool check = true;
 	while(check){
 		cout<<endl<<"Choose an option: ";
@@ -36,9 +98,6 @@ main(){
 				cin>>password;
 				break;
 			case 2:
-				cout<<"Create account code"<<endl;
-				break;
-			case 3:
 				cout<<"Goodbye!";
 				check = false;
 				break;
