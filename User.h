2b4e39a0e396d147f2e237120f1a8f67292e1bd9
ett642 <74976855+ett642@users.noreply.github.com>
2020-12-03 14:26:55 -0600

    Update User.h

--- User.h
+++ User.h
@@ -3,26 +3,31 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "CD.h"
+#include "CheckingAccount.h"
+#include "SavingsAccount.h"
 #include "Accounts.h"
 using namespace std;
 
 class User : public Accounts
 {
     private:
-    
-    vector<string> transactions;
+    string phone;
+	string address
 
 
     public:
     void accMenu();
+    void changePass();
 
-    User(  string num, string pass, string inName, string date, vector<string> transactions) : Accounts( num,  pass,  inName,  date)
+    User(string num, string pass, string inName, string date, string inPhone, string inAddress) : Accounts(num, pass, inName, date)
     {
-        transactions.clear();
+        phone = inPhone;
+	address = inAddress;
     }
 
     void loggedIn(string date){
-		cout<<"Successfully logged in! Last login was "<<lastDate<<endl;
+		cout << "Successfully logged in! Last login was " << lastDate << endl;
 		lastDate = date;
         int userInput;
 
@@ -64,6 +69,27 @@ class User : public Accounts
                 //CD
             }
         }
+    }
+    void changePass()
+    {
+        string input,input2;
+        cout << "Enter new password: ";
+        cin >> input;
+        cout << "Re-enter new password: ";
+        cin >> input2;
+        while(input != input2 )
+        {
+            cout << "Passwords do not match. Please Re-Enter new password. or enter 1 to restart password change.";
+            cin >> input2;
+            if(input2 == "1")
+            {
+                changePass();
+                exit(0);
+            }
+
+        }
+        // save password to file here.
+
     }
 };
 
