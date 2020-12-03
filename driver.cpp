30716ba8d6fcd5c374c6f5bd898541fcba12307b
ett642 <74976855+ett642@users.noreply.github.com>
2020-12-03 16:06:47 -0600

    File updater is done
    We need to include methods to write to functions in User and Official now

--- driver.cpp
+++ driver.cpp
@@ -23,16 +23,21 @@ void getDate(string date, int *month, int *day, int *year){
 
 void fromFile(vector<Accounts> &logins){
 	fstream file; string num; string pass; string name; string lastDate; string text;
+	logins.clear();
 	file.open("logins.txt");
 	if(file){
+		cout<<"Reading from file..."<<endl;
 		while(getline(file,text)){
 			if(text[0] == '0'){
 				num = text.substr(0, text.find("*"));
 				text = text.substr(text.find("*")+1);
+
 				pass = text.substr(0, text.find("*"));
 				text = text.substr(text.find("*")+1);
+
 				name = text.substr(0, text.find("*"));
 				text = text.substr(text.find("*")+1);
+
 				lastDate = text;
 				Admin temp = Admin(num,pass,name,lastDate);
 				logins.push_back(temp);
@@ -41,37 +46,65 @@ void fromFile(vector<Accounts> &logins){
 				string status;
 				num = text.substr(0, text.find("*"));
 				text = text.substr(text.find("*")+1);
+				
 				pass = text.substr(0, text.find("*"));
 				text = text.substr(text.find("*")+1);
+				
 				name = text.substr(0, text.find("*"));
 				text = text.substr(text.find("*")+1);
+				
 				lastDate = text.substr(0, text.find("*"));
 				text = text.substr(text.find("*")+1);
+				
 				status = text;
 				Official temp = Official(num,pass,name,lastDate,status);
 				logins.push_back(temp);
 			}
 			else{
+				string phone; string address;
 				num = text.substr(0, text.find("*"));
 				text = text.substr(text.find("*")+1);
+				
 				pass = text.substr(0, text.find("*"));
 				text = text.substr(text.find("*")+1);
+				
 				name = text.substr(0, text.find("*"));
 				text = text.substr(text.find("*")+1);
-				lastDate = text;
+				
+				lastDate = text.substr(0, text.find("*"));
+				text = text.substr(text.find("*")+1);
+				
+				phone = text.substr(0, text.find("*"));
+				text = text.substr(text.find("*")+1);
+				
+				address = text;
+				User temp = User(num,pass,name,lastDate,phone,address);
+				logins.push_back(temp);
 			}
 		}
 	}
 	file.close();
 }
 
+void updateFile(vector<Accounts> &logins){
+	fstream file; file.open("logins.txt",fstream::trunc);
+	cout<<"Updating file...\n";
+	for(Accounts &i: logins){
+		file<<i.printToFile()<<endl;
+	}
+	file.close();
+	fromFile(logins);
+}
+
 void loginSearch(string number, string password, vector<Accounts> &logins){
-	for(Account &i: logins){
-		if(number == i.getNumber() and password == i.getPassword){
+	for(Accounts &i: logins){
+		if(number == i.getNumber() and password == i.getPassword()){
 			cout<<"Logged in."<<endl;
+			updateFile(logins);
 			return;
 		}
 	}
+	cout<<"Login not found.\n";
 	
 }
 
@@ -96,6 +129,7 @@ main(){
 				cin>>loginNum;
 				cout<<"Enter your login password: ";
 				cin>>password;
+				loginSearch(loginNum,password,logins);
 				break;
 			case 2:
 				cout<<"Goodbye!";
