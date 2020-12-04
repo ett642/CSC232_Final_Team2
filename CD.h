f6d8319f02b69e3319f664ef04464b909a7ed363
ethan2151 <72755849+ethan2151@users.noreply.github.com>
2020-12-03 19:21:49 -0600

    Update CD.h
    Updated a menu and worked on code throughout

--- CD.h
+++ CD.h
@@ -13,29 +13,109 @@ using namespace std;
 class CD : public AbAccount {
     public:
     double term;
-    double annualRate;
-    bool status;
-   // Default constructor
-   CD() : AbAccount()
+    double rate;
+    bool flag;
+    // Constructor
+    CD (string num, string date, double balance, bool status, bool flag): AbAccount(num, date, balance,status)
+    {
+       term = 3;
+       rate = 0.1;
+       flag = true;
+    }
+   //function for logged in menu
+   void menu(string date)
    {
-      double term = 3;
-      double rate = 0.1;
-      bool status = true
-   }
-   // 2nd Constructor
-   CD (string num, double balance, double interestRate): 
-   AbAccount (num, balance, interestRate)
-   {
-      term = 3;
-      rate = 0.1;
-      status = true
-   }
+	//cout<<"Successfully logged in! Last login was "<<lastDate<<endl;
+	//lastDate = date;
+   for(;;) { // menu for  loop
+                    cout << "-------------------" << endl;
+                    cout << "What would you like to do? \n" << endl;
+                    cout << "[1] Open a CD" << endl;
+                    cout << "[2] Withdraw Funds from an Existing CD" << endl;
+                    cout << "[3] Check CD Info" << endl;
+                    cout << "[4] Return to Main Menu" << endl;
+                    cout << "-------------------" << endl;
+                    int userInput;
+                    cin >> userInput;
+                    switch(userInput) {
+                       case 1: 
+                       {
+                          cout << "-------------------" << endl;
+                          cout << "How much would you like to deposit?" << endl;
+                          cout << "-------------------" << endl;
+                          long double depositAmt;
+                          cin >> depositAmt;
+                          deposit(depositAmt);
+                          
+                          //deposit amount using depositAmt?
+                          
+                          cout << "-------------------" << endl;
+                          cout << "Your CD balance is: " << balance << endl;
+                          cout << "-------------------" << endl;
+                          cout << "Hit enter to continue" << endl;
+                          cout << "-------------------" << endl;
+                          char enter = cin.get(); 
+                          cin.ignore();
+                          continue;
+                       }
+                       case 2: 
+                       {
+                          cout << "-------------------" << endl;
+                          cout << "How much would you like to Withdraw?" << endl;
+                          cout << "-------------------" << endl;
+                          long double withdrawAmt;
+                          cin >> withdrawAmt;
+                          withdraw(withdrawAmt);
+                          
+                          //withdraw amount using withdrawAmt?
+                          //fix withdraw function and work on how to close CD account
+                          
+                          cout << "-------------------" << endl;
+                          cout << "Your CD is now closed" << endl;
+                          cout << "-------------------" << endl;
+                          cout << "Hit enter to continue" << endl;
+                          char enter = cin.get(); 
+                          cin.ignore();
+                          continue;
+                       }
+                       case 3:
+                       {
+                          cout << "-------------------" << endl;
+                          cout << "Your Current CD Balance is: " << balance << endl;
+                          cout << "-------------------" << endl;
+                          cout << "Hit enter to continue" << endl;
+                          cout << "-------------------" << endl;
+                          char enter = cin.get(); 
+                          cin.ignore();
+                          continue;
+                       }
+                       case 4:
+                       {
+                          cout << "-------------------" << endl;
+                          cout << "Returning to Main Menu... " << endl;
+                          cout << "-------------------" << endl;
+                          return;
+                       }
+                       default: // error catching
+                       { 
+                          cout << "Try another input!" << endl;
+                          cin.clear();
+                          cin.ignore();
+                          continue;
+                       }
+                    }
+                 }
+    }
+
+   
+   
+  
    //function to open a CD
    void open(double amount) 
    {
       if(amount > 0.00) //if the amount deposited is more than 0 
       {
-         accBalance += amount;//successfully deposit
+         balance += amount;//successfully deposit
       }
       else //if the deposit is not a positive amount it will not deposit
       {
@@ -51,7 +131,7 @@ class CD : public AbAccount {
             break;
          }
       }
-      if(accBalance >= 0.0) //if the balance is greater than $0 the status will remain open 
+      if(balance >= 0.0) //if the balance is greater than $0 the status will remain open 
       {
          status = true;
       }
@@ -65,20 +145,20 @@ class CD : public AbAccount {
    {
        if(status == false) 
         { // withdraw with penalty
-            cout << "Warning! Your withdrawl will incur a penalty, your Certificate of Deposit balance is: $" << accBalance << endl;
+            cout << "Warning! Your withdrawl will incur a penalty, your Certificate of Deposit balance is: $" << balance << endl;
             cout << "You must wait until the maturity date to withdraw your funds with no penalty " << endl;
         }
         else if (status == true) 
         { // do the withdraw from the balance with no penalty
-            if(accBalance >= amount) 
+            if(balance >= amount) 
             {
-                accBalance -= amount; 
+                balance -= amount; 
             }
             else 
             {
                 cout << "Invalid funds " << endl;
             }
-            if(accBalance < 0.0)// if the balance falls below $0 account becomes inactive 
+            if(balance < 0.0)// if the balance falls below $0 account becomes inactive 
             {
                 cout << "Warning! You cannot withdraw your Certificate of Deposit has fallen below $0.00. " << endl;
                 cout << "You must deposit more money and wait until the maturity date before you can make another withdraw without penalty." << endl;
@@ -86,12 +166,12 @@ class CD : public AbAccount {
             }
         }
     }
-    void calcInterest(double accBalance, double annualRate)// calculates current interest
+    double calcInterest(double balance, double annualRate)// calculates current interest
     {
        double dailyRate = annualRate / 365;
-       dailyRate = dailyRate * accBalance;
-       accBalance += dailyRate;
-       return accBalance;
+       dailyRate = dailyRate * balance;
+       balance += dailyRate;
+       return balance;
     }      
 };
 #endif
