6b08a1e6820e9240d0b4b98d7a0fc2aed4ca9f58
lapis-subie6 <72754670+lapis-subie6@users.noreply.github.com>
2020-12-04 19:45:03 -0600

    Update Official.h

--- Official.h
+++ Official.h
@@ -2,6 +2,8 @@
 #define OFFICIAL_H
 #include <iostream>
 #include <string>
+#include <vector>
+#include "User.h"
 #include "Accounts.h"
 using namespace std;
 
@@ -23,7 +25,7 @@ public:
 			status = false;
 	}
 	
-	void loggedIn(string date){
+	void loggedIn(string date, vector<Accounts> &logins){
 		if(status){
 			cout<<"Status: Active"<<endl;
 		}
@@ -33,7 +35,76 @@ public:
 		}
 	}
 	
-	//Method to open and close bank accounts
+	void openClose(vector<Accounts> &logins)
+	{
+		string input;
+		int sNum;
+		cout << "Enter the customer account you would like to access: ";
+		cin >> input;
+		for(Accounts &i: logins){
+		if(i.getNumber() == input)
+			{
+				cout << "Which account type would you like to access?\n" << "1 for Checking, 2 for Savings, 3 for CD";
+				cin >> sNum;
+				for(;;)
+				{
+					switch(sNum)
+					{
+						case 1:
+						{
+							i.changeCheckingStat();
+						}
+						case 2:
+						{
+							i.changeSavingsStat();
+						}
+						case 3:
+						{
+							i.changeCDStat();
+						}
+						case 4:
+						{
+							cout << "-------------------" << endl;
+							cout << "Exiting Menu... " << endl;
+							cout << "-------------------" << endl;
+							return;
+						}
+						
+						default: // error catching
+						{ 
+							cout << "Try another input!" << endl;
+							cin.clear();
+							cin.ignore();
+							continue;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	void changeCheckingStat(User i)
+	{
+		CheckingAccount c;
+		c = i.getChecking();
+		c.setStatus();
+	}
+
+	void changeSavingsStat(User i)
+	{
+		SavingsAccount s;
+		s = i.getSavings();
+		s.setStatus();
+	}
+
+	void changeCDStat(User i)
+	{
+		CD c;
+		c = i.getCD();
+		c.setStatus();
+	}
+
+	
 	
 	//Method to retrieve info from closed accounts
 	
