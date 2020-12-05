77207c987d8fb5e1eb5962a5648b625e3c3f2f9d
lapis-subie6 <72754670+lapis-subie6@users.noreply.github.com>
2020-12-04 22:19:11 -0600

    Update Official.h

--- Official.h
+++ Official.h
@@ -41,10 +41,12 @@ public:
 		int sNum;
 		cout << "Enter the customer account you would like to access: ";
 		cin >> input;
-		for(Accounts &i: logins){
-		if(i.getNumber() == input)
+		for(Accounts &i: logins)
+		{
+			if(i.getNumber() == input)
 			{
-				cout << "Which account type would you like to access?\n" << "1 for Checking, 2 for Savings, 3 for CD";
+				cout << "Which account type would you like to access?\n" << "1 for Checking\n" << "2 for Savings\n";
+				cout <<"3 for CD\n" << "4 to exit\n";
 				cin >> sNum;
 				for(;;)
 				{
@@ -52,15 +54,15 @@ public:
 					{
 						case 1:
 						{
-							i.changeCheckingStat();
+							changeCheckingStat(i);
 						}
 						case 2:
 						{
-							i.changeSavingsStat();
+							changeSavingsStat(i);
 						}
 						case 3:
 						{
-							i.changeCDStat();
+							changeCDStat(i);
 						}
 						case 4:
 						{
@@ -69,7 +71,7 @@ public:
 							cout << "-------------------" << endl;
 							return;
 						}
-						
+							
 						default: // error catching
 						{ 
 							cout << "Try another input!" << endl;
@@ -81,46 +83,225 @@ public:
 				}
 			}
 		}
+		cout << "Customer not found.\n";
+		
 	}
 
-	void changeCheckingStat(User i)
+	void changeCheckingStat(Accounts i)
 	{
 		CheckingAccount c;
 		c = i.getChecking();
 		c.setStatus();
+		if(c.getStatus() == false)
+		{
+			//save num, inName
+			// save account info
+		}
 	}
 
-	void changeSavingsStat(User i)
+	void changeSavingsStat(Accounts i)
 	{
 		SavingsAccount s;
 		s = i.getSavings();
 		s.setStatus();
+		if(s.getStatus() == false)
+		{
+			//save num, inName
+			// save account info
+		}
 	}
 
-	void changeCDStat(User i)
+	void changeCDStat(Accounts i)
 	{
 		CD c;
 		c = i.getCD();
 		c.setStatus();
+		if(c.getStatus() == false)
+		{
+			//save num, inName
+			// save account info
+		}
 	}
 
-	
-	
-	//Method to retrieve info from closed accounts
-	
-	//deposit and withdraw from a user account with permission
-	
-	//Search Account by name, number, and phone number
-	
-	
-	
-	void changeStatus(){
-		if(s == "t"){
-			s ="f";
-		else{
-			s="t";
+	void accountDeposit(vector<Accounts> &logins)
+	{
+		string input;
+		CheckingAccount c;
+		SavingsAccount s;
+		CD cd;
+		int sNum;
+		cout << "Enter the customer account you would like to access: ";
+		cin >> input;
+		for(Accounts &i: logins)
+		{
+			
+			if((i.getNumber() == input))
+			{
+				cout << "Allow the Customer to enter their password.\n";
+				cin >> input;
+				if(i.getPassword() == input)
+				{
+					cout << "Which account type would you like to access?\n" << "1 for Checking\n" << "2 for Savings\n";
+					cout <<"3 for CD\n" << "4 to exit\n";
+					cin >> sNum;
+					for(;;)
+					{
+						switch(sNum)
+						{
+							case 1:
+							{
+								c = i.getChecking();
+								cout << "1 to deposit\n" << "2 to withdraw" << "3 to Cancel";
+								cin >> sNum;
+								for(;;)
+								{
+									switch(sNum)
+									{
+										case 1:
+										{
+											cout << "-------------------" << endl;
+											cout << "How much would you like to deposit?" << endl;
+											cout << "-------------------" << endl;
+											long double depositAmt;
+											cin >> depositAmt;
+											c.deposit(depositAmt);
+										}
+										case 2:
+										{
+											cout << "-------------------" << endl;
+											cout << "How much would you like to Withdraw?" << endl;
+											cout << "-------------------" << endl;
+											long double withdrawAmt;
+											cin >> withdrawAmt;
+											c.withdraw(withdrawAmt);
+										}
+										case 3:
+										{
+											cout << "-------------------" << endl;
+											cout << "Exiting Menu... " << endl;
+											cout << "-------------------" << endl;
+											return;
+										}
+											
+										default: // error catching
+										{ 
+											cout << "Try another input!" << endl;
+											cin.clear();
+											cin.ignore();
+											continue;
+										}
+									}
+								}
+							}
+							case 2:
+							{
+								s = i.getSavings();
+								cout << "1 to deposit\n" << "2 to withdraw" << "3 to Cancel";
+								cin >> sNum;
+								for(;;)
+								{
+									switch(sNum)
+									{
+										case 1:
+										{
+											cout << "-------------------" << endl;
+											cout << "How much would you like to deposit?" << endl;
+											cout << "-------------------" << endl;
+											long double depositAmt;
+											cin >> depositAmt;
+											s.deposit(depositAmt);
+										}
+										case 2:
+										{
+											cout << "-------------------" << endl;
+											cout << "How much would you like to Withdraw?" << endl;
+											cout << "-------------------" << endl;
+											long double withdrawAmt;
+											cin >> withdrawAmt;
+											s.withdraw(withdrawAmt);
+										}
+										case 3:
+										{
+											cout << "-------------------" << endl;
+											cout << "Exiting Menu... " << endl;
+											cout << "-------------------" << endl;
+											return;
+										}
+											
+										default: // error catching
+										{ 
+											cout << "Try another input!" << endl;
+											cin.clear();
+											cin.ignore();
+											continue;
+										}
+									}
+								}
+							}
+							case 3:
+							{
+								//add CD when CD is complete
+							}
+							case 4:
+							{
+								cout << "-------------------" << endl;
+								cout << "Exiting Menu... " << endl;
+								cout << "-------------------" << endl;
+								return;
+							}
+								
+							default: // error catching
+							{ 
+								cout << "Try another input!" << endl;
+								cin.clear();
+								cin.ignore();
+								continue;
+							}
+						}
+					}
+				}
+				else
+				{
+					cout << "Password incorrect.\n";
+					return;
+				}
+				
+			}
 		}
-		createStatus(s);
+		cout << "Customer not found.\n";
+		
 	}
+
+	void searchAccount(vector<Accounts> &logins)
+	{
+		string input;
+		CheckingAccount c;
+		SavingsAccount s;
+		CD cd;
+		int sNum;
+		cout << "Enter the customer account you would like to access: ";
+		cin >> input;
+		for(Accounts &i: logins)
+		{
+			
+			if((i.getNumber() == input) || (i.getName() == input) || (i.getPhone() == input))
+			{
+				cout << i.getNumber() << " " << i.getName() << " " << i.getPhone() << endl;
+			}
+		}
+	}
+
+	void changeStatus(){
+		if(stat == "t"){
+			stat ="f";
+		}
+		else{
+			stat="t";
+		}
+		createStatus(stat);
+	}
+	
+
+	//Method to retrieve info from closed accounts
 };
 #endif
